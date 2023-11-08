56c0fa865dcad3839aa879d398535f26d2865087
Pierre Silva de Sá <88999985+pierresilva1@users.noreply.github.com>
2023-11-07 21:55:30 -0300

    fim

--- src/paginas/Equipamentos.jsx
+++ src/paginas/Equipamentos.jsx
@@ -64,6 +64,7 @@ function Equipamento() {
                 <p>A pastilha Peltier é um cooler termoelétrico utilizado em purificadores de água, bebedouros e mini refrigeradores de diversas marcas. Também pode ser empregado em projetos alternativos de refrigeração de computadores. Sua alta performance garante, em poucos minutos, o aquecimento ou resfriamento de objetos.</p>
                 <a href="https://www.eletrogate.com/pastilha-termoeletrica-peltier-tec1-12706" className="amodals">compre aqui</a>
                 </div>
+                <br />
             </Peltier_Modal>
             <Rele_Modal isOpen={openRele_Modal} setRele_ModalOpen={() => setRele_ModalOpen(!openRele_Modal)}>
                 <div className="modalsls">
@@ -72,6 +73,7 @@ function Equipamento() {
                 <p>O módulo relé 1 canal funciona com tensão de 5 V, e pode acionar cargas de até 250 VAC ou 30 VDC, suportando uma corrente máxima de 10 A. Possui led indicador de energia, 2 pinos de energia e 1 de controle, além do borne de saída com parafusos, facilitando a conexão dos equipamentos.</p>
                 <a href="https://www.eletrogate.com/modulo-rele-2-canais-5v-10a-com-borne-kr" className="amodals">compre aqui</a>
                 </div>
+                <br />
             </Rele_Modal>
             <P4kre_Modal isOpen={openP4kre_Modal} setP4kre_ModalOpen={() => setP4kre_ModalOpen(!openP4kre_Modal)}>
                 <div className="modalsls">
@@ -80,6 +82,7 @@ function Equipamento() {
                 <p>Este conector é usado para ligar a alimentação das câmeras de segurança CFTV. Produto com excelente qualidade profissional e de fácil instalação: Não precisa soldar, pois ele tem parafusos que facilitam a fixação do cabo bastando ter uma chave Philips para poder prendê-los.</p>
                 <a href="https://www.eletrogate.com/conector-adaptador-plug-p4-femea-com-borne" className="amodals">compre aqui</a>
                 </div>
+                <br />
             </P4kre_Modal>
 
             <Fonte_Modal isOpen={openFonte_Modal} setFonte_ModalOpen={() => setFonte_ModalOpen(!openFonte_Modal)}>
@@ -89,6 +92,7 @@ function Equipamento() {
                 <p>O sistema bivolt é uma característica que dá aos equipamentos eletrônicos a possibilidade de ligá-los tanto em uma rede de 110V quanto em uma de 220V, sem o risco de comprometer a integridade dos aparelhos.</p>
                 <a href="https://produto.mercadolivre.com.br/MLB-2831517951-fonte-12v-1a-bivolt-pcftv-fita-barra-de-led-_JM#is_advertising=true&position=2&search_layout=stack&type=pad&tracking_id=05f01d85-065d-472c-bbb0-0bfdc220ca83&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=2&ad_click_id=NzlkNmM3NDYtNTI1OC00OTU0LTkyMzItNDk0NzU4YjUwMzQ1" className="amodals">compre aqui</a>
                 </div>
+                <br />
             </Fonte_Modal>
             <Interruptor_Modal isOpen={openInterruptor_Modal} setInterruptor_ModalOpen={() => setInterruptor_ModalOpen(!openInterruptor_Modal)}>
                 <div className="modalsls">
@@ -97,6 +101,7 @@ function Equipamento() {
                 <p>O interruptor é um dispositivo simples criado pelo médico Golding Bird, aproximadamente na década de 1840, utilizado na abertura de circuitos elétricos em redes, tomadas e entradas de aparelhos eletrônicos, basicamente na maioria das situações que envolvem o ligamento ou desligamento de energia elétrica.</p>
                 <a href="https://www.eletrogate.com/chave-gangorra-com-2-terminais" className="amodals">compre aqui</a>
                 </div>
+                <br />
             </Interruptor_Modal>
         </section>
         <br />
